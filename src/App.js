6b50f54eb117461c1b02dfabba395a24fb807669
Kris Coverdale <kris.coverdale@uk.ibm.com>
2017-04-23 12:29:37 +0100

    add styling

--- src/App.js
+++ src/App.js
@@ -20,6 +20,10 @@ const list = [
   },
 ];
 
+const largeColumn = {width: '40%',};
+const mediumColumn = {width: '30%',};
+const smallColumn = {width: '10%',};
+
 const isSearched = (searchTerm) => (item) =>
  !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
@@ -30,16 +34,16 @@ const Search = ({ value, onChange, children }) =>
   </form>
 
 const Table = ({ list, pattern, onDismiss }) =>
-  <div>
+  <div className="table">
     {
       list.filter(isSearched(pattern)).map(item =>
-        <div key={item.objectID}>
-          <span><a href={item.url}>{item.title}</a></span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-          <span>
-            <Button onClick={() => onDismiss(item.objectID)}>
+        <div key={item.objectID} className="table-row">
+          <span style={largeColumn}><a href={item.url}>{item.title}</a></span>
+          <span style={mediumColumn}>{item.author}</span>
+          <span style={smallColumn}>{item.num_comments}</span>
+          <span style={smallColumn}>{item.points}</span>
+          <span style={smallColumn}>
+            <Button onClick={() => onDismiss(item.objectID)} className="button-inline">
               Dismiss
             </Button>
           </span>
@@ -80,19 +84,20 @@ class App extends Component {
   render() {
     const { searchTerm, list } = this.state;
     return (
-      <div className="App">
-        <Search
-          value={searchTerm}
-          onChange={this.onSearchChange}
-        >
-        Search
-        </Search>
-        <Table
-          list={list}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
-        />
-
+      <div className="page">
+        <div className="interactions">
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          >
+          Search
+          </Search>
+          <Table
+            list={list}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+          />
+        </div>
       </div>
     );
   }
