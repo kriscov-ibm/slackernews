611cc58c66608e9c8977a17df62a352d7edbc716
Kris Coverdale <kris.coverdale@uk.ibm.com>
2017-04-23 10:13:52 +0100

    basic state example, remove items from list

--- src/App.js
+++ src/App.js
@@ -20,15 +20,38 @@ const list = [
   },
 ];
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+    };
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    //returns a new list.  Filters out the id that was passed in.
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList});
+  }
+
   render() {
     return (
       <div className="App">
-        { list.map(item =>
+        { this.state.list.map(item =>
             <div key={item.objectID}>
               <span><a href={item.url}>{item.title}</a></span>
               <span>{item.author}</span>
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
+              <span>
+                <button onClick={() => this.onDismiss(item.objectID)} type="button">
+                  Dismiss
+                </button>
+              </span>
             </div>
         )}
       </div>
