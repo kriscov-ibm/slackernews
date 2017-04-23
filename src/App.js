3543f37df9d4b4773eb523fb88809dca1f86fd29
Kris Coverdale <kris.coverdale@uk.ibm.com>
2017-04-23 09:29:04 +0100

    iterate a list and output

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'http://kriscoverdale.com',
+    author: 'Kris Coverdale',
+    num_comments: 9,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactsjs/redux',
+    author: 'Dan Abramov, Andrew Clarke',
+    num_comments: 5,
+    points: 5,
+    objectID: 1,
+  },
+];
 class App extends Component {
   render() {
-    var helloWorld = 'Welcome to variable React';
     return (
       <div className="App">
-        <h2>{helloWorld}</h2>
+        { list.map(function(item) {
+          return <div>{item.title}</div>;
+        })}
       </div>
     );
   }
