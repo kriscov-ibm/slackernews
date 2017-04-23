97da42b71ec19d2d91fbfb69e46920d5b3acb7b8
Kris Coverdale <kris.coverdale@uk.ibm.com>
2017-04-23 11:59:11 +0100

    refactor to functional components stage 1

--- src/App.js
+++ src/App.js
@@ -23,54 +23,48 @@ const list = [
 const isSearched = (searchTerm) => (item) =>
  !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
-class Search extends Component {
-  render() {
-    const { value, onChange, children } = this.props;
-    return (
-      <form>
-        {children}
-        <input type="text" value={value} onChange={onChange} />
-      </form>
-    );
-  }
+function Search (props) {
+  const { value, onChange, children } = props;
+  return (
+    <form>
+      {children}
+      <input type="text" value={value} onChange={onChange} />
+    </form>
+  );
 }
 
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-    return (
-      <div>
-        {
-          list.filter(isSearched(pattern)).map(item =>
-            <div key={item.objectID}>
-              <span><a href={item.url}>{item.title}</a></span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-                <Button onClick={() => onDismiss(item.objectID)}>
-                  Dismiss
-                </Button>
-              </span>
-            </div>
-        )}
-      </div>
-    );
-  }
+function Table (props) {
+  const { list, pattern, onDismiss } = props;
+  return (
+    <div>
+      {
+        list.filter(isSearched(pattern)).map(item =>
+          <div key={item.objectID}>
+            <span><a href={item.url}>{item.title}</a></span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <Button onClick={() => onDismiss(item.objectID)}>
+                Dismiss
+              </Button>
+            </span>
+          </div>
+      )}
+    </div>
+  );
 }
 
-class Button extends Component {
-  render() {
-    const { onClick,
-            className = '', 
-            children
-          } = this.props;
-    return (
-      <button onClick={onClick} className={className} type="button">
-        {children}
-      </button>
-    );
-  }
+function Button (props) {
+  const { onClick,
+          className = '',
+          children
+        } = props;
+  return (
+    <button onClick={onClick} className={className} type="button">
+      {children}
+    </button>
+  );
 }
 
 class App extends Component {
